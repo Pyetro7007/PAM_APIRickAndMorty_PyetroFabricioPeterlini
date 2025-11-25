2c324087a2ca61f1fd1b61c0b8ffbb02c177b9d2
Pyetro7007 <pyetrofabricio2014@gmail.com>
2025-11-25 19:43:57 -0300

    Consertar erros

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import CharacterDetailScreen from './src/screens/CharactersListScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="CharacterList">
+                <Stack.Screen name="CharacterList" component={CharactersListScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
--- src/screens/CharactersListScreen.js
+++ src/screens/CharactersListScreen.js
@@ -1,25 +1,131 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, ActivityIndicator, Image, TouchableOpacity, StyleSheet } from "react-native";
+
+const CharacterCard = ({ character, navigation }) => (
+    <TouchableOpacity style={styles.card} 
+    //onPress={() => navigation.navigate("CharacterDetail", { characterId: character.id })}
+    OnPress={() => console.log("Clicado:", character.name)}
+    >
+
+    <Image
+        source={{ uri: character.image }}
+        style={styles.image}
+    />
+    <View style={styles.infoContainer}>
+        <Text style={styles.name} numberOfLines={1}> {character.name} </Text>
+        <Text style={styles.details}>
+            <Text style={{ fontWeight: "bold" }}> Status: </Text> {character.status}
+        </Text>
+
+        <Text style={styles.details}>
+            <Text style={{ fontWeight: "bold" }}> Espécie: </Text> {character.species}
+        </Text>
+    </View>
+    </TouchableOpacity>
+);
+
 const CharactersListScreen = ({ navigation }) => {
     const [characters, setCharacters] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        loadCharacters();
+        const fetchCharacters = async () => {
+            try {
+                const response = await api.get("/character");
+
+                setCharacters(response.data.results);
+                setLoading(false);
+            } catch (error) {
+                console.error("Erro ao buscar personagens:", error);
+                setError("Não foi possível carregar os personagens");
+                setLoading(false);
+            }
+        };
+
+        fetchCharacters();
     }, []);
 
-    const loadCharacters = async () => {
-        try {
-            setLoading(true)
-            const data = await fetchCharacters(1);
-            setCharacters(data);
-            setLoading(false);
-        } catch (error) {
-            console.error("Falaha ao carregar os dados:", error);
-            setError("Não foi possível carregra os personagens. Tente Novamente");
-            setLoading(false);
-            Alert.alert("Erro", "Houve um problema ao buscar os dados da API")
-        }
-    };
+    if (loading) {
+        return (
+            <View style={styles.center}>
+                <ActivityIndicator size="large" color="#000000"/>
+                <Text>Buscando personagens...</Text>
+            </View>
+        );
+    }
 
-    if
-}
+    if (error) {
+        return (
+            <View style={styles.center}>
+                <Text style={styles.errorText}> {error} </Text>
+            </View>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <FlatList data={characters} keyExtractor={(item) => String(item.id)} renderItem={({ item }) => (
+                <CharacterCard character={item} navigation={navigation} />
+            )}
+            contentContainerStyle={styles.listContent}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    paddingTop: 10,
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 16,
+  },
+  listContent: {
+    paddingHorizontal: 10,
+  },
+  card: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+    alignItems: 'center',
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+  infoContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  details: {
+    fontSize: 14,
+    color: '#666',
+  },
+});
+
+export default CharactersListScreen;
